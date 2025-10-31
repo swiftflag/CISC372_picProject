4dedb1e107a3c3c28527a57712b250be58456187
Jackson Singley <jaxsing@r0login1.localdomain.hpc.udel.edu>
2025-10-31 18:37:02 -0400

    final

--- image.c
+++ image.c
@@ -60,7 +60,7 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
     span=srcImage->bpp*srcImage->bpp;
-#pragma omp parallel for num_threads(100) private(pix, bit)
+#pragma omp parallel for num_threads(16) private(pix, bit)
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
