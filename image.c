2a9bd0349a665eb95540ee033e6e78ab7d532160
jaxsing <jaxsing@udel.edu>
2025-10-31 15:57:26 -0400

    donezo?

--- image.c
+++ image.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string.h>
 #include "image.h"
+#include <omp.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -59,6 +60,7 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
     span=srcImage->bpp*srcImage->bpp;
+#pragma omp parallel for num_threads(100) private(pix, bit)
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
@@ -67,7 +69,6 @@ void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
         }
     }
 }
-
 //Usage: Prints usage information for the program
 //Returns: -1
 int Usage(){
@@ -111,7 +112,10 @@ int main(int argc,char** argv){
     destImage.height=srcImage.height;
     destImage.width=srcImage.width;
     destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
+    double start = omp_get_wtime();
     convolute(&srcImage,&destImage,algorithms[type]);
+    double end = omp_get_wtime();       // stop timer
+    printf("Convolution took %.4f seconds\n", end - start);
     stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
     stbi_image_free(srcImage.data);
     
